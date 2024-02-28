01b37759e555ce6d3576b6a6cc3c4be4bbdb165d
Ahmed Khalid <ahmedkhalidahi22@gmail.com>
2024-02-28 13:04:10 +0300

    update subtitle color

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="relative flex flex-col gap-7 text-center items-center text-gray-950">
         <h1 className="font-extrabold text-7xl">What The Hook?</h1>
-        <p className=" text-xl font-normal">
+        <p className=" text-xl font-normal text-gray-800">
           Your interactive guide to mastering{" "}
           <span className="font-bold">React Hooks</span>
         </p>
